95c4d8c577aa71d7038898b3cdd4e5ee4f91a249
Hassan Murtaza <mrhassanmurtaza@gmail.com>
2019-04-03 19:37:01 +0500

    Creating conflict

--- simple_calculator.py
+++ simple_calculator.py
@@ -7,17 +7,16 @@ def subtract(num1, num2):
 def multiply(num1, num2):
   return num1 * num2
 
-
 # Testing it out
-def square(num):
+def triangle(num):
   return num * num
 
 def divide(num1, num2):
   return num1/num2
 
-
-print(add(5,6))
-print(subtract(6,5))
-print(multiply(5,6))
-print(square(6))
-print(divide(6,4))
+if __name__ == "__main__":
+  print(add(512,56))
+  print(subtract(65,45))
+  print(multiply(41,123))
+  print(square(66))
+  print(divide(6,4))
