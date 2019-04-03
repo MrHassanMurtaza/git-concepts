db82cf71de987c812b793bf4799893686687d26a
Hassan Murtaza <mrhassanmurtaza@gmail.com>
2019-04-03 19:08:36 +0500

    adding calculator functions

---
+++ simple_calculator.py
@@ -0,0 +1,17 @@
+def add(a, b):
+  return a + b
+
+def subtract(a, b):
+  return a - b
+
+def multiply(a, b):
+  return a * b
+
+def divide(a, b):
+  return a/b
+
+
+print(add(5,6))
+print(subtract(6,5))
+print(multiply(5,6))
+print(divide(5,6))
